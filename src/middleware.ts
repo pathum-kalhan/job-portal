1cec4c0ec207a2b9382d850dfba85d0388b262a4
Dilan <101853864+dilan95x@users.noreply.github.com>
2024-03-01 18:52:14 +0530

    midelware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,29 @@
-import { withAuth } from "next-auth/middleware";
+export { default } from "next-auth/middleware";
 
-export default withAuth(
+// export default withAuth(
   
-  { 
-  callbacks: {
-    authorized: ({ req, token }) => {
+//   { 
+//   callbacks: {
+//     authorized: ({ req, token }) => {
       
-      const { pathname } = req.nextUrl;
-      const employerMatch = pathname.match(/^\/dashboard\/employer\/?(.*)/);
-      const candidateMatch = pathname.match(/^\/dashboard\/candidate\/?(.*)/); 
+//       const { pathname } = req.nextUrl;
+//       const employerMatch = pathname.match(/^\/dashboard\/employer\/?(.*)/);
+//       const candidateMatch = pathname.match(/^\/dashboard\/candidate\/?(.*)/); 
 
-      if (Boolean(token)) {
+//       if (Boolean(token)) {
 
-        if (employerMatch) {
-          return token?.role === "employer";
-        } else if (candidateMatch) {
-          return token?.role === "candidate";
-        }
-      } 
+//         if (employerMatch) {
+//           return token?.role === "employer";
+//         } else if (candidateMatch) {
+//           return token?.role === "candidate";
+//         }
+//       } 
 
-      return Boolean(token)
+//       return Boolean(token)
      
-    },
-  },
-});
+//     },
+//   },
+// });
 
 export const config = {
   matcher: ["/dashboard/:path*"],
