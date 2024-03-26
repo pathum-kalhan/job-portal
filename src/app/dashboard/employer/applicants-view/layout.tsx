8875379e1b56ee52a1dd66300a73e672659e27ee
Dilan <101853864+dilan95x@users.noreply.github.com>
2024-03-26 12:15:07 +0530

    last commit

--- src/app/dashboard/employer/applicants-view/layout.tsx
+++ src/app/dashboard/employer/applicants-view/layout.tsx
@@ -1,5 +1,6 @@
-import { Card, Grid, Typography } from "@mui/material";
+import { Card, Grid, Stack, Typography } from "@mui/material";
 import type { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: "Applications",
@@ -19,6 +20,21 @@ export default function Layout({
             <Typography fontWeight="bold">Applicants View</Typography>
           </Card>
         
+        </Grid>
+
+        <Grid item xs={11} mt={2}>
+          <Stack alignItems="center" justifyContent="center">
+            <Image
+              src={"/bg_images/manage_aplicants.webp"}
+              alt="Welcome to Career Guide Pro"
+              priority={true}
+              layout="responsive"
+              objectFit="contain"
+              objectPosition="center"
+              width={1520}
+              height={300}
+            />
+          </Stack>
         </Grid>
       </Grid>
 
