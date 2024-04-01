b6468b842f17a52fe9bab5988d5fd3f3ac31e032
Sasiru Ravihansa <sasiruravihansa@gmail.com>
2024-04-01 17:07:28 +0530

    updated name show

--- src/app/dashboard/candidate/chat/page.tsx
+++ src/app/dashboard/candidate/chat/page.tsx
@@ -44,47 +44,49 @@ export default function Page() {
           <CircularProgress />
         </Stack>
       ) : (
-          <>
-        {employers.length ? (<Grid container spacing={2}>
-          <Grid item xs={2}>
-            <Box sx={{ display: "flex", flexDirection: "column" }}>
-              {employers.map((employer) => (
-                <Button
-                  key={employer._id}
-                  onClick={() => {
-                    setSelectedEmployerId(employer._id);
-                  }}
-                >
-                  <Grid container spacing={1}>
-                    <Grid item xs={4}>
-                      <Avatar src={employer?.profilePic?.image} />
-                    </Grid>
-                    <Grid item>
-                      <Typography
-                        sx={{
-                          fontSize: "1.2rem",
-                          fontWeight: "bold",
-                          textAlign: "center",
-                        }}
-                      >
-                        {employer?.name}
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </Button>
-              ))}
-            </Box>
-          </Grid>
+        <>
+          {employers.length ? (
+            <Grid container spacing={2}>
+              <Grid item xs={2}>
+                <Box sx={{ display: "flex", flexDirection: "column" }}>
+                  {employers.map((employer) => (
+                    <Button
+                      key={employer._id}
+                      onClick={() => {
+                        setSelectedEmployerId(employer._id);
+                      }}
+                    >
+                      <Grid container spacing={1}>
+                        <Grid item xs={4}>
+                          <Avatar src={employer?.profilePic?.image} />
+                        </Grid>
+                        <Grid item>
+                          <Typography
+                            sx={{
+                              fontSize: "1.2rem",
+                              fontWeight: "bold",
+                              textAlign: "center",
+                            }}
+                          >
+                            {employer?.name?.split(" ")[0]}
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                    </Button>
+                  ))}
+                </Box>
+              </Grid>
 
-          <Grid item xs={10}>
-            <Chat employeeId={id} employerId={selectedEmployerId} />
-          </Grid>
-            </Grid>):(
-             <Stack alignItems="center" justifyContent="center">
-             <Typography variant="h5">Inbox is empty.</Typography>
-           </Stack>
-            )}
-            </>
+              <Grid item xs={10}>
+                <Chat employeeId={id} employerId={selectedEmployerId} />
+              </Grid>
+            </Grid>
+          ) : (
+            <Stack alignItems="center" justifyContent="center">
+              <Typography variant="h5">Inbox is empty.</Typography>
+            </Stack>
+          )}
+        </>
       )}
     </>
   );
