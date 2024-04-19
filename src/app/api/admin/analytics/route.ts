dd8f2fcb5cf620a7520bf5ca3ce975db422eee34
Dilan <101853864+dilan95x@users.noreply.github.com>
2024-04-19 16:32:56 +0530

    analytics part 2

--- src/app/api/admin/analytics/route.ts
+++ src/app/api/admin/analytics/route.ts
@@ -5,6 +5,7 @@ import AdminModel from "../../models/Admin";
 import CandidateModel from "../../models/Candidate";
 import EmployerModel from "../../models/Employer";
 import JobPostModel from "../../models/JobPost";
+import ApplicationModel from "../../models/Application";
 
 export async function POST(request: Request) {
   try {
@@ -51,13 +52,65 @@ export async function POST(request: Request) {
         query.createdAt.$lte = endDate;
       }
     }
-
+    // counts with filtering
     const employersRegistrationCount = await EmployerModel.countDocuments(
       query
     );
     const candidatesRegistrationCount = await CandidateModel.countDocuments(
       query
     );
+
+    // Calculate the monthly average registration count for candidates
+    const candidateMonthlyAvgReg = await CandidateModel.aggregate([
+      {
+        $group: {
+          _id: {
+            year: { $year: "$createdAt" },
+            month: { $month: "$createdAt" },
+          },
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalMonths: { $sum: 1 },
+          totalCount: { $sum: "$count" },
+        },
+      },
+      {
+        $project: {
+          monthlyAverage: { $divide: ["$totalCount", "$totalMonths"] },
+        },
+      },
+    ]);
+
+    // Calculate the monthly average registration count for employers
+    const employerMonthlyAvgReg = await EmployerModel.aggregate([
+      {
+        $group: {
+          _id: {
+            year: { $year: "$createdAt" },
+            month: { $month: "$createdAt" },
+          },
+          count: { $sum: 1 },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalMonths: { $sum: 1 },
+          totalCount: { $sum: "$count" },
+        },
+      },
+      {
+        $project: {
+          monthlyAverage: { $divide: ["$totalCount", "$totalMonths"] },
+        },
+      },
+    ]);
+
+    // Total counts
     const activeTotalEmployerCount = await EmployerModel.countDocuments({
       profileStatus: "active",
     });
@@ -68,11 +121,129 @@ export async function POST(request: Request) {
       },
     });
 
+    const successRateCount = await ApplicationModel.aggregate([
+      {
+        $match: {
+          cvReviewStatus: "shortListed",
+        },
+      },
+      {
+        $lookup: {
+          from: "jobposts",
+          localField: "job",
+          foreignField: "_id",
+          as: "job",
+        },
+      },
+      {
+        $lookup: {
+          from: "candidates",
+          localField: "candidate",
+          foreignField: "_id",
+          as: "candidate",
+        },
+      },
+      {
+        $unwind: "$job",
+      },
+      {
+        $unwind: "$candidate",
+      },
+      {
+        $addFields: {
+          matchingSkills: {
+            $filter: {
+              input: "$job.requiredQualifications",
+              as: "reqQualification",
+              cond: {
+                $in: ["$$reqQualification", "$candidate.skills"],
+              },
+            },
+          },
+        },
+      },
+      {
+        $match: {
+          $expr: {
+            $and: [
+              {
+                $eq: [
+                  "$job.industry",
+                  { $arrayElemAt: ["$candidate.industry", 0] },
+                ],
+              },
+              { $ne: ["$matchingSkills", []] },
+            ],
+          },
+        },
+      },
+      {
+        $count: "count",
+      },
+    ]);
+    
+
+    const activeCandidatesCount = await CandidateModel.countDocuments({
+      profileStatus: "active",
+    });
+
+    const candidateSuccessRate =
+      (successRateCount[0].count / activeCandidatesCount) * 100;
+
+    const employerSuccessRates = await JobPostModel.aggregate([
+      {
+        $lookup: {
+          from: "applications",
+          localField: "_id",
+          foreignField: "job",
+          as: "applications",
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          totalJobPosts: { $sum: 1 },
+          totalShortlistedCandidates: {
+            $sum: {
+              $size: {
+                $filter: {
+                  input: "$applications",
+                  as: "app",
+                  cond: { $eq: ["$$app.cvReviewStatus", "shortListed"] },
+                },
+              },
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          overallSuccessRate: {
+            $multiply: [
+              { $divide: ["$totalShortlistedCandidates", "$totalJobPosts"] },
+              100,
+            ],
+          },
+        },
+      },
+    ]);
+
     const responseData = {
-      employersRegistrationCount,
-      candidatesRegistrationCount,
-      activeTotalEmployerCount,
-      activeJobs,
+      employersRegistrationCount: employersRegistrationCount ?? 0,
+      candidatesRegistrationCount: candidatesRegistrationCount ?? 0,
+      activeTotalEmployerCount: activeTotalEmployerCount ?? 0,
+      activeJobs: activeJobs ?? 0,
+      jobMatchedAndShortlisted: successRateCount[0]?.count ?? 0,
+      candidateSuccessRate: `${Math.ceil(candidateSuccessRate ?? 0)}%`,
+      EmployerSuccessRate: `${Math.ceil(
+        employerSuccessRates[0]?.overallSuccessRate ?? 0
+      )}%`,
+      candidateMonthlyAvgReg: Math.floor(
+        candidateMonthlyAvgReg[0]?.monthlyAverage ?? 0
+      ),
+      employerMonthlyAvgReg: Math.floor(
+        employerMonthlyAvgReg[0]?.monthlyAverage ?? 0
+      ),
     };
 
     return NextResponse.json(
@@ -85,6 +256,7 @@ export async function POST(request: Request) {
       }
     );
   } catch (error: any) {
+    console.log(error);
     return NextResponse.json(
       {
         message: error,
